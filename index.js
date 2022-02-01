565cb442a0a116846ea4f4bf48e6cfd59e9cb5d1
Sweta Kumari Singh <swetasingh7076@gmail.com>
2022-02-02 02:53:17 +0530

    setup view engine

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use('/', require('./routes'));
 
 //setup view engine
 app.set('view engine', 'ejs');
-app.set('views', './views')
+app.set('views', './views');
 app.listen(port, function(err){
     if(err){
         console.log(`error  in running server: ${err}`);    //interpolation
