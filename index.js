8cffd8dd93db28e66919145b94dd959fd6ffa2c8
Sweta Kumari Singh <swetasingh7076@gmail.com>
2022-02-02 02:51:37 +0530

    setup all directory file including ejs

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//use express router
 app.use('/', require('./routes'));
+
+//setup view engine
+app.set('view engine', 'ejs');
+app.set('views', './views')
 app.listen(port, function(err){
     if(err){
         console.log(`error  in running server: ${err}`);    //interpolation
