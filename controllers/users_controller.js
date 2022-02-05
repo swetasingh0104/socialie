b68d61191f69fe05eae523080ca72dc91df2801e
Sweta Kumari Singh <swetasingh7076@gmail.com>
2022-02-06 02:22:06 +0530

    changes in sign-up user controller

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -23,49 +23,27 @@ module.exports.signIn = function(req, res){
     });
 }
 
-//get the sign-up data
-// module.exports.create = function(req, res){
-//     if(req.body.password != req.body.confirm_password){
-//        return res.redirect('back');
-//     }
-//     User.findOne({email: req.body.email}, function(err, user){
-//         if(err){console.log('error in finding user in signing up')}
-
-//         if(!user){
-//             User.create(req.body, function(err,user){
-//                 if(err){console.log('error in creating user while in signing up');
-//                  return}
-
-//                  return res.redirect('/users/sign-in')
-                
-//             })
-//         }else{
-//             return res.redirect('back');
-//         }
-//     })
-    
-// }
+// get the sign-up data
 module.exports.create = function(req, res){
-    if (req.body.password != req.body.confirm_password){
-        console.log("pssword");
-        return res.redirect('back');
-       
+    if(req.body.password != req.body.confirm_password){
+       return res.redirect('back');
     }
-
     User.findOne({email: req.body.email}, function(err, user){
-        if(err){console.log('error in finding user in signing up'); return}
+        if(err){console.log('error in finding user in signing up')}
 
-        if (!user){
-            User.create(req.body, function(err, user){
-                if(err){console.log('error in creating user while signing up'); return}
-                console.log('got it');
-                return res.redirect('/users/sign-in');
+        if(!user){
+            User.create(req.body, function(err,user){
+                if(err){console.log('error in creating user while in signing up');
+                 return}
+
+                 return res.redirect('/users/sign-in')
+                
             })
         }else{
             return res.redirect('back');
         }
-
-    });
+    })
+    
 }
 
 
