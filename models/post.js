e2ed482c3ae700d9f61f1ee24b7353dc6f601e72
Sweta Kumari Singh <swetasingh7076@gmail.com>
2022-02-17 21:32:30 +0530

    adding comments and nested population

--- models/post.js
+++ models/post.js
@@ -1,20 +1,3 @@
-// const mongoose = require('mongoose');
-
-// const postSchema = new mongoose.Schema({
-//     content:{
-//         type:String,
-//         required:true
-//     },
-//     user:{
-//         type : mongoose.Schema.Types.ObjectId,
-//         ref: 'User'
-//     }
-// },{
-//     timestamps:true
-// });
-
-// const Post = mongoose.model('Post', postSchema);
-// module.exports = Post
 const mongoose = require('mongoose');
 
 
@@ -27,7 +10,14 @@ const postSchema = new mongoose.Schema({
         type:  mongoose.Schema.Types.ObjectId,
         ref: 'User'
 
-    }
+    },
+    // include the array of ids of all comments in this post schema itself
+    comments: [
+        {
+            type:  mongoose.Schema.Types.ObjectId,
+            ref: 'Comment'
+        }
+    ]
 },{
     timestamps: true
 });
