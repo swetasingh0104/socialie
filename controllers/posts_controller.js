66db66e50f9592d30d2b51eb9f39361fdf434997
Sweta Kumari Singh <swetasingh7076@gmail.com>
2022-03-25 00:49:39 +0530

    added flash messages

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -8,12 +8,12 @@ module.exports.create = async function(req, res){
             content: req.body.content,
             user: req.user._id
         });
-    
+        req.flash('success', 'Post published!');
         return res.redirect('back');
 
     }catch(err){
-        console.log('ERROR', err);
-        return;
+        req.flash('error', err);
+        return res.redirect('back');
     }
    
 }
@@ -24,19 +24,22 @@ module.exports.destroy = async function(req, res){
     try{
         let post = await Post.findById(req.params.id);
 
-         if(post.user == req.user.id){
+        if(post.user == req.user.id){
              post.remove();
 
              await Comment.deleteMany({post: req.params.id});
-                 return res.redirect('back');
 
-         }else{
+             req.flash('success', 'Post and associated comments deleted!');
+
+                 return res.redirect('back');
+        }else{
+            req.flash('error', 'You cannot delete this post!');
              return res.redirect('back');
          }
 
     }catch{
-        console.log('ERROR', err);
-        return;
+        req.flash('error', err);
+        return res.redirect('back');
 
     }
    
