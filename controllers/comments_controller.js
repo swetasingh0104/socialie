ff43912cfaeee083c8e201f478a1fd4803922dca
Sweta Kumari Singh <swetasingh7076@gmail.com>
2022-04-05 03:17:56 +0530

    converted comment to ajax

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -16,15 +16,28 @@ module.exports.create = async function(req, res){
             post.comments.push(comment);
             post.save();
 
+            if (req.xhr){
+                // Similar for comments to fetch the user's id!
+                comment = await comment.populate('user', 'name').execPopulate();
+
+                return res.status(200).json({
+                    data: {
+                        comment: comment
+                    },
+                    message: "Post created!"
+                });
+            }
+
+
+            req.flash('success', 'Comment published!');
+
              res.redirect('/');
         }
     }catch(err){
         console.log('Error', err);
         return;
-        
-    }
+     }
    
-    
 }
 
 module.exports.destroy = async function(req, res){
@@ -37,10 +50,24 @@ module.exports.destroy = async function(req, res){
             comment.remove();
 
            let post = await Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}});
-                return res.redirect('back');
+                
+           // send the comment id which was deleted back to the views
+           if (req.xhr){
+            return res.status(200).json({
+                data: {
+                    comment_id: req.params.id
+                },
+                message: "Post deleted"
+            });
+        }
+
+        req.flash('success', 'Comment deleted!');
+
+        return res.redirect('back');
            
         }
         else{
+            req.flash('error', 'Unauthorized');
             return res.redirect('back');
         }
 
