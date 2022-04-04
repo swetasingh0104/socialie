c4d523fcd77714974373f36b22b3ee77db3a737f
Sweta Kumari Singh <swetasingh7076@gmail.com>
2022-04-05 02:45:03 +0530

    converting to ajax-sending data

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -2,9 +2,8 @@ const Post = require('../models/post');
 const Comment = require('../models/comment');
 
 module.exports.create = async function(req, res){
-
     try{
-        await Post.create({
+       let post =    await Post.create({
             content: req.body.content,
             user: req.user._id
         });
@@ -39,6 +38,16 @@ module.exports.destroy = async function(req, res){
 
              await Comment.deleteMany({post: req.params.id});
 
+
+             if (req.xhr){
+                return res.status(200).json({
+                    data: {
+                        post_id: req.params.id
+                    },
+                    message: "Post deleted"
+                });
+            }
+            
              req.flash('success', 'Post and associated comments deleted!');
 
                  return res.redirect('back');
