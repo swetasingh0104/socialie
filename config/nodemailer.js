da3040c0e5c5818e256cbf36d71646342ff99d98
Sweta Kumari Singh <swetasingh7076@gmail.com>
2022-07-21 01:05:28 +0530

    added nodemailer

---
+++ config/nodemailer.js
@@ -0,0 +1,78 @@
+const nodemailer = require("nodemailer");
+const ejs = require('ejs');
+const path = require('path')
+
+
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    auth: {
+        user: 'swetaa0104@gmail.com',
+        pass: 'dotxgqvldkdgdvkr'
+    }
+});
+
+
+let renderTemplate = (data, relativePath) => {
+    let mailHTML;
+    ejs.renderFile(
+        path.join(__dirname, '../views/mailers', relativePath),
+        data,
+        function(err, template){
+         if (err){console.log('error in rendering template'); return}
+         
+         mailHTML = template;
+        }
+    )
+
+    return mailHTML;
+}
+
+
+module.exports = {
+    transporter: transporter,
+    renderTemplate: renderTemplate
+}
+
+
+
+
+// const nodemailer = require("nodemailer");
+// const ejs = require('ejs');
+// const path = require('path')
+
+
+// let transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     host: 'smtp.gmail.com',
+//     port: 587,
+//     secure: false,
+//     auth: {
+//         user: 'swetaa0104',
+//         pass: 'defaultemail#'
+//     }
+// });
+
+
+// let renderTemplate = (data, relativePath) => {
+//     let mailHTML;
+//     ejs.renderFile(
+//         path.join(__dirname, '../views/mailers', relativePath),
+//         data,
+//         function(err, template){
+//          if (err){console.log('error in rendering template'); return}
+         
+//          mailHTML = template;
+//         }
+//     )
+
+//     return mailHTML;
+// }
+
+
+// module.exports = {
+//     transporter: transporter,
+//     renderTemplate: renderTemplate
+// }
--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,25 +1,27 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
-
+const commentsMailer = require('../mailers/comments_mailer');
 module.exports.create = async function(req, res){
-   
+
     try{
         let post = await Post.findById(req.body.post);
 
         if (post){
-             let comment = await  Comment.create({
+             let comment = await Comment.create({
                 content: req.body.content,
                 post: req.body.post,
                 user: req.user._id
             });
-                 
-            post.comments.push(comment);
-            post.save();
 
+            post.comments.push(comment);
+           await post.save();
+            
+           comment = await comment.populate('user', 'name email');
+         commentsMailer.newComment(comment);
             if (req.xhr){
                 // Similar for comments to fetch the user's id!
-                comment = await comment.populate('user', 'name').execPopulate();
-
+                
+    
                 return res.status(200).json({
                     data: {
                         comment: comment
@@ -31,50 +33,51 @@ module.exports.create = async function(req, res){
 
             req.flash('success', 'Comment published!');
 
-             res.redirect('/');
+            res.redirect('/');
         }
     }catch(err){
-        console.log('Error', err);
+        console.log(err.message);
+        req.flash('error', err);
         return;
-     }
-   
+    }
+    
 }
 
+
 module.exports.destroy = async function(req, res){
+
     try{
-        let comment = await Comment.findById(req.params.id)
+        let comment = await Comment.findById(req.params.id);
+
         if (comment.user == req.user.id){
 
             let postId = comment.post;
 
             comment.remove();
 
-           let post = await Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}});
-                
-           // send the comment id which was deleted back to the views
-           if (req.xhr){
-            return res.status(200).json({
-                data: {
-                    comment_id: req.params.id
-                },
-                message: "Post deleted"
-            });
-        }
+            let post = Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}});
 
-        req.flash('success', 'Comment deleted!');
+            // send the comment id which was deleted back to the views
+            if (req.xhr){
+                return res.status(200).json({
+                    data: {
+                        comment_id: req.params.id
+                    },
+                    message: "Post deleted"
+                });
+            }
 
-        return res.redirect('back');
-           
-        }
-        else{
+
+            req.flash('success', 'Comment deleted!');
+
+            return res.redirect('back');
+        }else{
             req.flash('error', 'Unauthorized');
             return res.redirect('back');
         }
-
     }catch(err){
-        console.log('Error', err);
+        req.flash('error', err);
         return;
     }
-    
     
 }
