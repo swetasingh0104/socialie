6836c36f34537552aceeb85bca79d8fe286ec3af
Sweta Kumari Singh <swetasingh7076@gmail.com>
2022-07-22 02:27:58 +0530

    added nodemailer template

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -10,7 +10,7 @@ let transporter = nodemailer.createTransport({
     secure: false,
     auth: {
         user: 'swetaa0104@gmail.com',
-        pass: 'dotxgqvldkdgdvkr'
+        pass: 'jtwmwzjitfmpvlmr'
     }
 });
 
@@ -21,7 +21,7 @@ let renderTemplate = (data, relativePath) => {
         path.join(__dirname, '../views/mailers', relativePath),
         data,
         function(err, template){
-         if (err){console.log('error in rendering template'); return}
+         if (err){console.log('error in rendering template', err); return}
          
          mailHTML = template;
         }
